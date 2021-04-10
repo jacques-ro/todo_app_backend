0bffb3826ad1a85cbcb97a73bad757188f70a827
Jacques Roesen <jacques.roesen@googlemail.com>
2021-04-10 19:43:57 +0200

    feat: made some changes to the project structure
    - added containerization
    - added the option to include a appsettings.user.json
    - removed the https endpoint that is exposed by default
    - updated the readme file

--- Program.cs
+++ Program.cs
@@ -18,6 +18,10 @@ namespace backend
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddJsonFile("appsettings.user.json", true, true);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # todo_app_backend
 backend of a c# based todo app used as a lean learning platform
+
+# Commands to start the app
+
+dotnet run
+
+# How to customize appsettings
+
+Copy the original appsettings.Development.json to a new appsettings.user.json. Don't push your `appsettings.user.json` to the repository.
+
+## How to change the exposed http port
+
+Change the port setting in `appsettings.user.json`
